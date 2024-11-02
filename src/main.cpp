1842f800a28efc25c5e04a102aaf18f128c614d2
shichongchen <shichongchen896@gmail.com>
2024-11-02 11:59:55 +0800

    读取obj模型显示

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,10 @@
 #include <GLFW/glfw3.h>
 #include <stdexcept>
 #include <vector>
+#include <fstream>
+#include <iostream>
+#include <string>
+#include <sstream>
 
 struct Vertex
 {
@@ -13,27 +17,56 @@ struct Face
 	unsigned int a, b, c;
 };
 
-static std::vector<Vertex> vertices = {
-	{-0.5f, 0.5f, 0.0f},
-	{-0.5f, -0.5f, 0.0f},
-	{0.5f, -0.5f, 0.0f},
-	{0.5f, 0.5f, 0.0f}
-};
+static std::vector<Vertex> vertices;
 
-static std::vector<Face> faces = {
-	{0, 1, 2},
-	{0, 2, 3}
-};
+static std::vector<Face> faces;
 
-static void render()
+static void load_obj(std::string path)
+{
+	std::ifstream file(path);
+	if (!file.is_open())
+	{
+		std::cerr << "Failed to open file: " << path << '\n';
+		return;
+	}
+
+	std::string line;
+	while (std::getline(file, line))
+	{
+		if (line.substr(0, 2) == "v ")
+		{
+			std::istringstream s(line.substr(2));
+			Vertex vertex;
+			s >> vertex.x >> vertex.y >> vertex.z;
+			vertices.push_back(vertex);
+		} else if(line.substr(0, 2) == "f ") {
+			std::istringstream s(line.substr(2));
+			std::string sa, sb, sc;
+			Face face;
+			s >> sa >> sb >> sc;
+			std::istringstream(sa) >> face.a;
+			std::istringstream(sb) >> face.b;
+			std::istringstream(sc) >> face.c;
+			face.a--;
+			face.b--;
+			face.c--;
+			faces.push_back(face);
+ 		}
+	}
+
+	file.close();
+	std::cout << "Loaded " << vertices.size() << "vertices.\n";
+}
+
+static void draw_obj()
 {
 	glBegin(GL_TRIANGLES);
-
+	
 	for (auto const& face : faces)
 	{
-		Vertex a = vertices[face.a];
-		Vertex b = vertices[face.b];
-		Vertex c = vertices[face.c];
+		auto const& a = vertices.at(face.a);
+		auto const& b = vertices.at(face.b);
+		auto const& c = vertices.at(face.c);
 
 		glVertex3f(a.x, a.y, a.z);
 		glVertex3f(b.x, b.y, b.z);
@@ -85,11 +118,13 @@ int main()
 	printf("OpenGL version:", glGetString(GL_VERSION));
 
 	glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+	load_obj("C:\\Users\\scc\\Desktop\\opengl_-practise\\assets\\monkey.obj");
 	
 	while (!glfwWindowShouldClose(window))
 	{	
 		glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
-		render();
+		draw_obj();
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
